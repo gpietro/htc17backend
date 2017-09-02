dc8fbfa7cfafc35694044251dfd155bc76b00a36
Pietro Ghezzi <pietroghezzi.ch@gmail.com>
2017-09-02 11:53:23 +0000

    added car detection cascade + coordinates rest api

--- server.py
+++ server.py
@@ -10,7 +10,8 @@ import base64
 from tornado.options import define, options, parse_command_line
 from tornado.ioloop import PeriodicCallback
 
-face_cascade = cv2.CascadeClassifier('/home/pi/FTP/tornado-chat/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('/home/pi/FTP/htc17backend/haarcascade_frontalface_default.xml')
+car_cascade = cv2.CascadeClassifier('/home/pi/FTP/htc17backend/cars.xml')
 
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -21,6 +22,18 @@ class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.render('index.html')        
+
+
+class CoordinatesHandler(tornado.web.RequestHandler):    
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET')
+        
+    def get(self):
+        # Hardcoded coordinates
+        coordinates = [45.8367, 9.0246]
+        self.write({'coordinates': coordinates})
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
@@ -36,7 +49,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)        
 
         if message == "start":
-            self.camera_loop = PeriodicCallback(self.loop, 100)
+            self.camera_loop = PeriodicCallback(self.loop, 30)
             self.camera_loop.start()
         else:
             print("Unsupported function: " + message)
@@ -44,17 +57,20 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     @gen.coroutine
     def loop(self):
-        ret_val, imgread = self.cam.read()
-        img = cv2.resize(imgread, (self.width, self.height), interpolation = cv2.INTER_AREA)
+        ret_val, img = self.cam.read()
+        #img = cv2.resize(imgread, (self.width, self.height), interpolation = cv2.INTER_AREA)
 
-        cnt = cv2.imencode('.jpg', img)[1]
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in faces:
+        # faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        cars = car_cascade.detectMultiScale(gray, 1.3, 5)
+        #for (x, y, w, h) in faces:
+        #    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        print('cars %s' % len(cars))
+        for (x, y, w, h) in cars:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        try:
+        try:            
             ret, image = cv2.imencode('.jpg', img)
-            self.write_message(base64.b64encode(cnt))
+            self.write_message(base64.b64encode(image))
         except tornado.websocket.WebSocketClosedError:
             self.cam.release()
 
@@ -64,6 +80,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 app = tornado.web.Application([
     (r'/', IndexHandler),
+    (r'/coordinates', CoordinatesHandler),
     (r'/ws', WebSocketHandler),
 ])
 
